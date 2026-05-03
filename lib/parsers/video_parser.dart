c1bb2a421737e42911aae0c95270f400e3fcf75a
Gabriele Martina <contattagmstyle@gmail.com>
2026-05-03 18:26:22 +0200

    fix getPlaylistVideos parsing

--- lib/parsers/video_parser.dart
+++ lib/parsers/video_parser.dart
@@ -14,10 +14,12 @@ class VideoParser {
         name: traverseString(data, ["author"]) ?? '',
       ),
       duration: int.parse(
-          traverseString(data, ["videoDetails", "lengthSeconds"]) ?? '0'),
-      thumbnails: traverseList(data, ["videoDetails", "thumbnails"])
-          .map((item) => ThumbnailFull.fromMap(item))
-          .toList(),
+        traverseString(data, ["videoDetails", "lengthSeconds"]) ?? '0',
+      ),
+      thumbnails: traverseList(data, [
+        "videoDetails",
+        "thumbnails",
+      ]).map((item) => ThumbnailFull.fromMap(item)).toList(),
       unlisted: traverse(data, ["unlisted"]),
       familySafe: traverse(data, ["familySafe"]),
       paid: traverse(data, ["paid"]),
@@ -26,9 +28,10 @@ class VideoParser {
   }
 
   static VideoDetailed parseSearchResult(dynamic item) {
-    final columns = traverseList(item, ["flexColumns", "runs"])
-        .expand((e) => e is List ? e : [e])
-        .toList();
+    final columns = traverseList(item, [
+      "flexColumns",
+      "runs",
+    ]).expand((e) => e is List ? e : [e]).toList();
 
     final title = columns.firstWhere(isTitle, orElse: () => null);
     final artist = columns.firstWhere(isArtist, orElse: () => columns[1]);
@@ -44,42 +47,58 @@ class VideoParser {
         name: traverseString(artist, ["text"]) ?? '',
       ),
       duration: Parser.parseDuration(duration?['text']),
-      thumbnails: traverseList(item, ["thumbnails"])
-          .map((item) => ThumbnailFull.fromMap(item))
-          .toList(),
+      thumbnails: traverseList(item, [
+        "thumbnails",
+      ]).map((item) => ThumbnailFull.fromMap(item)).toList(),
     );
   }
 
   static VideoDetailed parseArtistTopVideo(
-      dynamic item, ArtistBasic artistBasic) {
+    dynamic item,
+    ArtistBasic artistBasic,
+  ) {
     return VideoDetailed(
       type: "VIDEO",
       videoId: traverseString(item, ["videoId"]) ?? '',
       name: traverseString(item, ["runs", "text"]) ?? '',
       artist: artistBasic,
       duration: null,
-      thumbnails: traverseList(item, ["thumbnails"])
-          .map((item) => ThumbnailFull.fromMap(item))
-          .toList(),
+      thumbnails: traverseList(item, [
+        "thumbnails",
+      ]).map((item) => ThumbnailFull.fromMap(item)).toList(),
     );
   }
 
   static VideoDetailed? parsePlaylistVideo(dynamic item) {
-    final flexColumns =
-        traverseList(item, ['flexColumns', 'runs']).expand((e) => e).toList();
-    final fixedColumns =
-        traverseList(item, ['fixedColumns', 'runs']).expand((e) => e).toList();
+    final flexColumns = traverseList(item, [
+      'flexColumns',
+      'runs',
+    ]).expand((e) => e is Iterable ? e : [e]).toList();
+    final fixedColumns = traverseList(item, [
+      'fixedColumns',
+      'runs',
+    ]).expand((e) => e is Iterable ? e : [e]).toList();
 
-    final title = flexColumns.firstWhere(isTitle,
-        orElse: () => flexColumns.isNotEmpty ? flexColumns[0] : null);
-    final artist = flexColumns.firstWhere(isArtist,
-        orElse: () => flexColumns.length > 1 ? flexColumns[1] : null);
+    final title = flexColumns.firstWhere(
+      isTitle,
+      orElse: () => flexColumns.isNotEmpty ? flexColumns[0] : null,
+    );
+    final artist = flexColumns.firstWhere(
+      isArtist,
+      orElse: () => flexColumns.length > 1 ? flexColumns[1] : null,
+    );
     final duration = fixedColumns.firstWhere(isDuration, orElse: () => null);
 
-    final videoId1 =
-        traverseString(item, ["playNavigationEndpoint", "videoId"]);
+    final videoId1 = traverseString(item, [
+      "playNavigationEndpoint",
+      "videoId",
+    ]);
     final videoId2 = RegExp(r"https:\/\/i\.ytimg\.com\/vi\/(.+)\/")
-        .firstMatch(traverseList(item, ["thumbnails"]).firstOrNull?.url ?? '')
+        .firstMatch(
+          (traverseList(item, ["thumbnails"]).firstOrNull as Map?)?['url']
+                  as String? ??
+              '',
+        )
         ?.group(1);
 
     if ((videoId1?.isEmpty ?? true) && videoId2 == null) {
@@ -94,10 +113,12 @@ class VideoParser {
         name: traverseString(artist, ["text"]) ?? '',
         artistId: traverseString(artist, ["browseId"]),
       ),
-      duration: Parser.parseDuration(duration?.text),
-      thumbnails: traverseList(item, ["thumbnails"])
-          .map((item) => ThumbnailFull.fromMap(item))
-          .toList(),
+      duration: Parser.parseDuration(
+        duration is Map ? duration['text'] : duration?.toString(),
+      ),
+      thumbnails: traverseList(item, [
+        "thumbnails",
+      ]).map((item) => ThumbnailFull.fromMap(item)).toList(),
     );
   }
 }
